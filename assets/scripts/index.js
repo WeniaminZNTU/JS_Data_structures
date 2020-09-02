4b27c80cdda209e2ab998139331801f00e93e03e
Veniamin <Weniaminwelichko@ukr.net>
2020-09-02 22:50:17 +0300

    Complited second task from JS Data structures

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,159 +1,132 @@
-"use strict";
+'use strict';
 
-//Task 2
-class ListNode {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-    this.prev = null;
+class Stack {
+  constructor(maxSize = 1000) {
+    if (typeof maxSize !== "number") {
+      throw new TypeError("size must be a number");
+    }
+    if (maxSize < 1) {
+      throw new RangeError("must be a positive number");
+    }
+
+    this._maxSize = maxSize;
+    this._size = 0;
+  }
+
+  get maxSize() {
+    return this._maxSize;
+  }
+
+  get isEmpty() {
+    return this.size === 0;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  push(value) {
+    if (this.size >= this.maxSize) {
+      throw new RangeError("Stack overflow");
+    }
+    this[this._size++] = value;
+    return this.size;
+  }
+
+  pop() {
+    const lastElement = this[--this._size];
+
+    delete this[this.size];
+
+    return lastElement;
+  }
+
+  peek() {
+    if (this.isEmpty) {
+      return;
+    }
+    return this[this.size - 1];
   }
 }
 
-class LinkedList {
-  constructor(...args) {
-    args = [...new Set(args)];
+// let stack = new Stack(string.length);
+console.log('checkBraces(\'()([<{}>])\'):',checkBraces('()([<{}>])'));
+console.log('checkBraces(\'()([<{}>]>\'):',checkBraces('()([<{}>]>'));
+console.log('checkBraces(\'()<{()}>([<{}>])\'):',checkBraces('()<{()}>([<{}>])'));
+console.log('checkBraces(\'()<{()}>[<{}>])\'):',checkBraces('()<{()}>[<{}>])'));
 
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
 
-    for (const item of args) {
-      if (!item || !Number.isInteger(item)) {
-        continue;
-      }
 
-      if (this.length === 0) {
-        const node = new ListNode(item);
-        this.head = node;
-        this.tail = node;
-        this.length++;
-      } else {
-        const newLastNode = new ListNode(item);
 
-        newLastNode.prev = this.tail;
-        this.tail.next = newLastNode;
-        this.tail = newLastNode;
-        this.length++;
-      }
-    }
-    return this.length;
+function checkBraces(string){
+  if(string.length % 2 !== 0){
+    return false;
   }
 
-    inputValidation(index, value){
-      if(!Number.isInteger(value)){
-        alert('ERROR: Value must be a number');
-        throw new TypeError('Value must be a number');
-      }
+  let stack = new Stack(string.length);
 
-      if(index < 0 || index > this.length){
-        throw new RangeError('Not in list');
-      }
 
-      if(index === 0 && this.length === 0){
-        const node = new ListNode(value);
+  for (let ch of string) {
 
-        this.head = node;
-        this.tail = node;
-        this.length++;
-        return;
-      }
-
-      if(index === 0 && this.length !== 0){
-        const node = new ListNode(value);
-
-        node.next = this.head;
-
-        this.head.prev = node;
-        this.head = node;
-        this.length++;
-        return 1;
-      }
-
-      for(let i = 0; i < this.length; i++){
-        if(value === this.peek(i)){
-          alert('ERROR: The number must not be repeated');
-          throw new RangeError('The number must not be repeated');
-        }
-      }
-
+    if(ch === '(' || ch === '[' || ch === '{' || ch === '<'){
+      // console.log('ch in if():',ch);
+      stack.push(ch);
+      // console.log('stack.peek():',stack.peek());
+      continue;
     }
 
-    insertAnte(index, value) {
-
-      this.inputValidation(index, value);
-
-      const node = new ListNode(value);
-      let currentNode = this.head;
-      let count = 0;
-
-      while(currentNode.next && count < index){
-        currentNode = currentNode.next;
-        count++;
+    switch(true){
+      case stack.peek() === '(' && ch === ')':{
+        stack.pop();
+      break;
       }
-
-      node.next = currentNode;
-      node.prev = currentNode.prev;
-
-      currentNode.prev = node;
-      currentNode.prev.next = node
-
-      return ++this.length;
+      case stack.peek() === '[' && ch === ']':{
+        stack.pop();
+      break;
+      }
+      case stack.peek() === '{' && ch === '}':{
+        stack.pop();
+      break;
+      }
+      case stack.peek() === '<' && ch === '>':{
+        stack.pop();
+      break;
+      }
+      // default:{
+      //   return false;
+      // }
     }
 
 
-    insertPost(index, value) {
+  }
 
-      this.inputValidation(index, value);
-
-      const node = new ListNode(value);
-      let currentNode = this.head;
-      let count = 0;
-
-      while(currentNode.next && count < index){
-        currentNode = currentNode.next;
-        count++;
-      }
-
-      node.next = currentNode.next;
-      node.prev = currentNode;
-
-      currentNode.next = node;
-      currentNode.next.prev = node;
-
-      return ++this.length;
-    }
-
-    peek(index=0) {
-      if(this.length === 0 || index < 0 || index > this.length){
-        throw new RangeError('Not in list');
-      }
-
-      let currentNode = this.head;
-      let count = (-1);
-      
-      while(count < index){
-        if(count + 1 === index){
-          return currentNode.value;
-        }
-        currentNode = currentNode.next;
-        count++;
-      }
-    }
-  
-    logValues(){
-      for(let i = 0; i < this.length; i++){
-        console.log(this.peek(i));
-      }
-    }
+  return stack.isEmpty;
 }
 
+// console.log(checkBraces('(((())))'))
 
-let lim = prompt('How many numbers do you want to enter?');
-const list = new LinkedList();
 
-for(let i = 0; i < lim; i++){
-  let number = +prompt(`Enter the ${i+1} number`);
-  list.insertPost(i,number);
+
+/* function checkBraces(string) {
+  const stack = new Stack(string.length);
+
+  if (string % 2 !== 0) {
+    return false;
+  }
+  for (const brace of string) {
+    if (brace === "(") {
+      stack.push(brace);
+      continue;
+    }
+
+    if (brace === ")") {
+      if (stack.isEmpty) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+  return stack.isEmpty;
 }
+ */
 
-list.logValues();
